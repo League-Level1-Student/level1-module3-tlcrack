1b527334f5bb52ffad8da0595ceddd647e0e993d
DylanArnold101 <league@WTS5.attlocal.net>
2019-02-05 18:04:59 -0800

    MagicBox

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
@@ -30,7 +31,8 @@ public class MagicBox extends JPanel implements Runnable, MouseListener {
 	 * 3. backgroundImage.getRGB(keyEvent.getX(), keyEvent.getY()) will give you the
 	 * color of the current pixel.
 	 */
-
+	JLabel lab = MP.loadImageFromWithinProject("door_meme?.jpg");
+	JFrame f = new JFrame();
 	BufferedImage backgroundImage;
 
 	public static void main(String[] args) throws Exception {
@@ -77,10 +79,16 @@ public class MagicBox extends JPanel implements Runnable, MouseListener {
 		// TODO Auto-generated method stub
 		System.out.println( backgroundImage.getRGB( e.getX(), e.getY() ) );
 		if(backgroundImage.getRGB( e.getX(), e.getY() )==-2451414||backgroundImage.getRGB( e.getX(), e.getY() )==-2451668||backgroundImage.getRGB( e.getX(), e.getY() )==-4738420) {
-			MP.loadImageFromWithinProject(door_meme?.jpg);
+			f.setVisible(true);
+			f.setSize(900, 650);
+			f.add(lab);
+			
 		}
 		else if(backgroundImage.getRGB( e.getX(), e.getY() )==-43351900||backgroundImage.getRGB( e.getX(), e.getY() )==-13726830||backgroundImage.getRGB( e.getX(), e.getY() )==-13458771) {
 			MP.speak("Thousands of gallons of gushing water! Do you have to go to the bathroom?");
+		}
+		else if(backgroundImage.getRGB(e.getX(), e.getY() )==-69429) {
+			MP.loadSound("moo.wav").play();
 		}
 		
 	}
