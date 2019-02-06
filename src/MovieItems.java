a90713743183c9bb33de09cac434f6554a87aeae
DylanArnold101 <league@WTS5.attlocal.net>
2019-02-05 18:27:49 -0800

    MovieStuff

--- src/MovieItems.java
+++ src/MovieItems.java
@@ -1,4 +1,13 @@
 
 public class MovieItems {
+	
+	public static void main(String[] args) {
+		Movie strange = new Movie("Doctor Strange",4);
+		Movie wackCondo = new Movie("Black Panther", 6);
+		Movie getTogether = new Movie("Infinity War",17000);
+		Movie buggers = new Movie("Ant Man and The Wasp",7);
+		Movie petey = new Movie("Guardians of the Galaxy",4);
+		
+	}
 
 }
